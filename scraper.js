d0aec47199ea97bc74a1dfdcb3abcde23dd01cda
Saatvik <insigniustwitch@gmail.com>
2022-04-04 20:11:22 +0530

    Review scarping complete

--- scraper.js
+++ scraper.js
@@ -32,14 +32,21 @@ const infotime = async () => {
     let price3 = prod_prices["2"].children[0].data
     
     
+    //Review SCARPING
+    let reviews = $('span[class="a-icon-alt"]') 
+      
+    let review1 = reviews["0"].children[0].data
+    let review2 = reviews["1"].children[0].data
+    let review3 = reviews["2"].children[0].data
     
-    
+   
+   
     let deetailz1 = []
     let deetailz2 = []
     let deetailz3 = []
-    deetailz1.push(title1,price1)
-    deetailz2.push(title2,price2)
-    deetailz3.push(title3,price3)
+    deetailz1.push(title1,price1,review1)
+    deetailz2.push(title2,price2,review2)
+    deetailz3.push(title3,price3,review3)
     info = {
 
         product_1: deetailz1,
@@ -51,3 +58,4 @@ const infotime = async () => {
     
 }
 infotime();
+//commit test
